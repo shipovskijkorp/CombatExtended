6a95cc3a11e232d920ea94a9b1bac83f2c66c6ab
shipovskijkorp <shipovskijkorparation@gmail.com>
2026-04-24 23:49:43 +0300

    start

---
+++ src/client/java/com/shipovskijkorp/combatextended/client/CombatExtendedClient.java
@@ -0,0 +1,10 @@
+package com.shipovskijkorp.combatextended.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CombatExtendedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/shipovskijkorp/combatextended/client/CombatExtendedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.shipovskijkorp.combatextended.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CombatExtendedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
