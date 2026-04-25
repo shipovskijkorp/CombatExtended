f0c4bf4a6420064ab83335f10c5224cb7c401bd1
shipovskijkorp <shipovskijkorparation@gmail.com>
2026-04-25 20:21:17 +0300

    totem rebalance

---
+++ src/main/java/com/shipovskijkorp/combatextended/mixin/accessor/EntityWorldAccessor.java
@@ -0,0 +1,12 @@
+package com.shipovskijkorp.combatextended.mixin.accessor;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Entity.class)
+public interface EntityWorldAccessor {
+    @Accessor("world")
+    World combatExtended$getWorld();
+}
